37fb7da1559677d37ef82f4c981cbd1b0902e59a
Michael Jones <iammichaeljon27@gmail.com>
2022-08-11 12:40:43 +0400

    Import images

--- src/App.js
+++ src/App.js
@@ -2,13 +2,42 @@ import DisplayMain from "./components/DisplayMain";
 import DisplayShop from "./components/DisplayShop";
 import DisplayHeader from "./components/DisplayHeader";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import uniqid from "uniqid";
+
+//import all images
+function importAll(r) {
+  let images = {};
+  r.keys().map(item => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
+
+const images = importAll(require.context('./components/images/shoes', false, /\.jpg/));
+const keys = Object.keys(images);
+let productname = []
+//format product name
+keys.forEach(key => {
+  let result = key.replaceAll("-", " ");
+  result = result.slice(0, -4)
+  productname.push(result)
+});
+
+//state array for products
+let arr = []
+keys.forEach((key, index) =>
+  arr.push({ id: uniqid(), link: images[key], name: productname[index] }))
+
+
+
 function App() {
+
+  const [products] = useState([arr])
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<DisplayHeader />}>
           <Route path="/" element={<DisplayMain />} />
-          <Route path="shop" element={<DisplayShop />} />
+          <Route path="shop" element={<DisplayShop items={products} />} />
         </Route>
       </Routes>
     </BrowserRouter>
