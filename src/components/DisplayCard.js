0f362ba9cc23fdd8e37d0c294bfd1dd2581f98d4
Michael Jones <iammichaeljon27@gmail.com>
2022-08-13 23:42:10 +0400

    Add styles for shop page
    Fix typo

--- src/components/DisplayCard.js
+++ src/components/DisplayCard.js
@@ -6,9 +6,9 @@ const DisplayCard = (props) => {
         <div className="product-card">
             <img src={prod.link}></img>
             <p className="prod-name">{prod.name}</p>
-            <p>${prod.price}</p>
-            <button data-price={prod.price} onClick={handler}
-                data-id={prod.id} data-src={prod.link} data-name={prod.name}>Add to card</button>
+            <p>{prod.price} USD</p>
+            <button className="add-cart" data-price={prod.price} onClick={handler}
+                data-id={prod.id} data-src={prod.link} data-name={prod.name}>Add to cart</button>
         </div>
 
 
--- src/components/DisplayShop.js
+++ src/components/DisplayShop.js
@@ -5,9 +5,9 @@ const DisplayShop = (props) => {
     return (
         <div className="shop-wrapper">
             <div className="navbar">
-                <div className="nav-items">item 1</div>
-                <div className="nav-items"> item 2</div>
-                <div className="nav-items"> item 3</div>
+                <div className="nav-items">SHOES</div>
+                <div className="nav-items"> SHIRTS</div>
+                <div className="nav-items"> PANTS</div>
             </div>
             <div className="shop-content">
                 {items.map((item) => {
