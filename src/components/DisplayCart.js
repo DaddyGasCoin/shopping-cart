5d311931eaa0d1b1ed6be416a94664776cd6a69d
Michael Jones <iammichaeljon27@gmail.com>
2022-08-14 18:18:41 +0400

    Add display for empty cart on cart page

--- src/components/DisplayCart.js
+++ src/components/DisplayCart.js
@@ -4,15 +4,25 @@ const DisplayCart = (props) => {
     const { data, price, handler } = props
     return (
         <>
-            <div className="cart-wrapper">
+            {price ? <> <div className="cart-wrapper">
                 {data.map((item) => {
                     return <DisplayProduct key={item.id} data={item} handler={handler} />
                 })}
             </div>
-            <div className="cart-foot">
-                <p className="price">TOTAL: {price} </p>
-                <button className="checkout-btn">CHECKOUT</button>
-            </div>
+                <div className="cart-foot">
+                    <p className="price">TOTAL: {price} </p>
+                    <button className="checkout-btn">CHECKOUT</button>
+                </div>
+            </> : <div className="empty-wrapper">
+                <div className="target">
+                    <p>cart is empty</p>
+                    <span className="material-symbols-sharp size-55">
+                        shopping_bag
+                    </span>
+                </div>
+
+            </div>}
+
         </>
     )
 }
