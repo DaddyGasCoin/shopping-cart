d58b3cdd63e58842057190c9f965f3110c37dcc3
Michael Jones <iammichaeljon27@gmail.com>
2022-08-14 16:30:02 +0400

    Add styles for home page

--- src/components/DisplayMain.js
+++ src/components/DisplayMain.js
@@ -1,10 +1,19 @@
-
+import img from './images/shoes/background/multi.jpg'
+import { Link } from "react-router-dom";
 
 const DisplayMain = () => {
     return (
-        <>
-            <div>Main Page</div>
-        </>
+        <div className="main-wrapper"
+            style={{
+                backgroundImage: `url(${img})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+            }}>
+            <Link className="text-link text" to="/shop">
+                <div className='shop-link'>SHOP NOW</div>
+            </Link>
+
+        </div>
     )
 }
 
